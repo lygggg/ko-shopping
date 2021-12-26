fefeda8c408bf70aee7bfd2c9f40f8343b0f0fc7
younggyulee <baayoo93@naver.com>
2021-12-26 17:24:28 +0900

    get home data

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,19 @@
 import React from "react";
+import axios from "axios";
+
+const getHome = async () => {
+  const { data } = await axios.get(
+    "https://c2c71aa1-a8be-4a3b-bd59-4b1498e1fe06.mock.pstmn.io/home"
+  );
+  console.log(data.themes);
+};
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      <button onClick={getHome}>클릭</button>
+    </div>
+  );
 };
 
 export default Home;
