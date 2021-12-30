ffba9ee8275ccf728b7ed2f05f248b0b84e41ed4
younggyulee <baayoo93@naver.com>
2021-12-31 08:30:16 +0900

    Add: 헤더파일을 추가했다

--- src/App.js
+++ src/App.js
@@ -3,14 +3,13 @@ import { BrowserRouter, Route, Link } from "react-router-dom";
 import Home from "./pages/Home";
 import Basket from "./pages/Basket";
 import Product from "./pages/Product";
+import Header from "./pages/Header";
 
 function App() {
   return (
     <BrowserRouter basename="/ko-shopping">
       <div>
-        <Link to="/">홈</Link>
-        <Link to="/productDetail">상품</Link>
-        <Link to="/basket">장바구니</Link>
+        <Header />
         <Route exact path="/" component={Home} />
         <Route path="/product" component={Product} />
         <Route path="/basket" component={Basket} />
---
+++ src/pages/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Header = () => {
+  return (
+    <>
+      <div>토멘코 쇼핑</div>
+      <div>장바구니</div>
+      <div>홈</div>
+    </>
+  );
+};
+
+export default Header;
