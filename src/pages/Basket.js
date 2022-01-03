7eb0c8ca97f578699cf1303a58fc8720cf8b724b
younggyulee <baayoo93@naver.com>
2022-01-04 03:48:47 +0900

    Feat: Basket 페이지의 css grid를 적용했다

--- src/pages/Basket.js
+++ src/pages/Basket.js
@@ -1,15 +1,46 @@
 import React from "react";
 
+import styled from "styled-components";
+
 const Basket = ({ product }) => {
   return (
     <>
-      <div>
-        <img src={product.thumbnail} />
-        <div>{product.name}</div>
-        <div>{product.price}</div>
-      </div>
+      <GridDiv>
+        <div>
+          <Img src={product.thumbnail} />
+        </div>
+        <NameDiv>{product.name}</NameDiv>
+        <div></div>
+        <PriceDiv>{product.price}원</PriceDiv>
+      </GridDiv>
     </>
   );
 };
+
+const GridDiv = styled.div`
+  width: 100%;
+  display: grid;
+  grid-template-columns: 180px 800px;
+  grid-column-gap: 100px;
+  padding: 30px;
+  border: solid 0.2px #eaeaea;
+  margin-bottom: 30px;
+`;
+
+const NameDiv = styled.div`
+  font-size: 30px;
+`;
+
+const PriceDiv = styled.div`
+  align-items: center;
+  font-size: 20px;
+  text-align: right;
+  font-size: 25px;
+`;
+
+const Img = styled.img`
+  width: 250px;
+  height: 180px;
+`;
 
 export default Basket;
--- src/pages/BasketList.js
+++ src/pages/BasketList.js
@@ -2,6 +2,8 @@ import React from "react";
 
 import Basket from "./Basket";
 
+import styled from "styled-components";
+
 const BasketList = () => {
   const basketList = {
     products: [
@@ -10,26 +12,34 @@ const BasketList = () => {
         name: "기모 트레이닝복 상하",
         description: "따듯한 트레이닝 복을 상하 세트로 만나보세요",
         thumbnail:
-          "http://img.1300k.com/events/site/2021/11/211108_131283_pc_11_1118.jpg",
+          "https://cdn.pixabay.com/photo/2016/11/30/08/46/living-room-1872191_960_720.jpg",
+        price: 12000,
       },
       {
         id: 12,
         name: "부드러운 극세사머플러 목도리선물",
         description: "여대생 방한템 부드러운 극세사머플러 목도리선물이에요",
         thumbnail:
-          "https://shopping-phinf.pstatic.net/main_2943080/29430808847.20211028014245.jpg?type=f640",
+          "https://cdn.pixabay.com/photo/2016/11/30/08/46/living-room-1872191_960_720.jpg",
+        price: 12000,
       },
     ],
   };
   return (
-    <div>
+    <MainDiv>
       {basketList.products.map((product) => (
         <div key={product.id}>
           <Basket product={product}></Basket>
         </div>
       ))}
-    </div>
+    </MainDiv>
   );
 };
 
+const MainDiv = styled.div`
+  height: 100vh;
+  display: flex;
+  flex-wrap: wrap;
+  flex-direction: column; /*수직 정렬*/
+`;
 export default BasketList;
