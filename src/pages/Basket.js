85a9f0867d97ec3add9839d770163bdba2f23e6c
younggyulee <baayoo93@naver.com>
2022-01-04 02:32:51 +0900

    Feat: 해당 장바구니 상품의 데이터를 출력했다

--- src/pages/Basket.js
+++ src/pages/Basket.js
@@ -1,26 +1,15 @@
 import React from "react";
 
-const basketList = {
-  products: [
-    {
-      id: 13,
-      name: "기모 트레이닝복 상하",
-      description: "따듯한 트레이닝 복을 상하 세트로 만나보세요",
-      thumbnail:
-        "http://img.1300k.com/events/site/2021/11/211108_131283_pc_11_1118.jpg",
-    },
-    {
-      id: 12,
-      name: "부드러운 극세사머플러 목도리선물",
-      description: "여대생 방한템 부드러운 극세사머플러 목도리선물이에요",
-      thumbnail:
-        "https://shopping-phinf.pstatic.net/main_2943080/29430808847.20211028014245.jpg?type=f640",
-    },
-  ],
-};
-
-const Basket = () => {
-  return <></>;
+const Basket = ({ product }) => {
+  return (
+    <>
+      <div>
+        <img src={product.thumnail} />
+        <div>{product.name}</div>
+        <div>{product.price}</div>
+      </div>
+    </>
+  );
 };
 
 export default Basket;
--- src/pages/BasketList.js
+++ src/pages/BasketList.js
@@ -1,5 +1,7 @@
 import React from "react";
 
+import Basket from "./Basket";
+
 const BasketList = () => {
   const basketList = {
     products: [
@@ -9,6 +11,7 @@ const BasketList = () => {
         description: "따듯한 트레이닝 복을 상하 세트로 만나보세요",
         thumbnail:
           "http://img.1300k.com/events/site/2021/11/211108_131283_pc_11_1118.jpg",
+        price: 13000,
       },
       {
         id: 12,
@@ -16,6 +19,7 @@ const BasketList = () => {
         description: "여대생 방한템 부드러운 극세사머플러 목도리선물이에요",
         thumbnail:
           "https://shopping-phinf.pstatic.net/main_2943080/29430808847.20211028014245.jpg?type=f640",
+        price: 13000,
       },
     ],
   };
@@ -24,7 +28,9 @@ const BasketList = () => {
     <>
       <div>
         {basketList.products.map((product) => (
-          <div key={product.id}></div>
+          <div key={product.id}>
+            <Basket product={product}></Basket>
+          </div>
         ))}
       </div>
     </>
